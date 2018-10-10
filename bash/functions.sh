35dd0406e9f3adb62102b9b0c77ce7f7ebc22be5
Lukas Reineke <lukas.reineke@protonmail.com>
2018-10-10 18:28:25 +0900

    scripts

--- bash/functions.sh
+++ bash/functions.sh
@@ -379,3 +379,26 @@ function ccrypt {
     fi
 }
 
+function exc {
+    if [ -f "$1" ]; then
+        case $1 in
+            *.tar.xz)   tar -xvf "$1"                         ;;
+            *.tar.bz2)  tar -jxvf "$1"                        ;;
+            *.tar.gz)   tar -zxvf "$1"                        ;;
+            *.bz2)      bunzip2 "$1"                          ;;
+            *.dmg)      hdiutil mount "$1"                    ;;
+            *.gz)       gunzip "$1"                           ;;
+            *.tar)      tar -xvf "$1"                         ;;
+            *.tbz2)     tar -jxvf "$1"                        ;;
+            *.tgz)      tar -zxvf "$1"                        ;;
+            *.zip)      unzip "$1"                            ;;
+            *.pax)      pax -r < "$1"                         ;;
+            *.pax.z)    uncompress "$1" --stdout | pax -r     ;;
+            *.rar)      7z x "$1"                             ;;
+            *.z)        uncompress "$1"                       ;;
+            *.7z)       7z x "$1"                             ;;
+            *)          echo "'$1' cannot be extracted/mounted via extract()" ;;
+        esac
+    fi
+}
+
