3b95081bef8fa84fc12dc2977fd805b90b3af4f1
Lukas Reineke <lukas.reineke@protonmail.com>
2021-05-16 12:23:07 +0900

    cleaned up rename

--- vim/lua/lsp/rename.lua
+++ vim/lua/lsp/rename.lua
@@ -4,19 +4,9 @@ local rename_prompt = "Rename -> "
 
 M.rename = function()
     local current_name = vim.fn.expand("<cword>")
-    local orig_bufnr = vim.api.nvim_get_current_buf()
     local bufnr = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_option(bufnr, "buftype", "prompt")
     vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
-    vim.api.nvim_buf_set_var(
-        bufnr,
-        "rename_vars",
-        {
-            current_name = current_name,
-            params = vim.lsp.util.make_position_params(),
-            orig_bufnr = orig_bufnr
-        }
-    )
     vim.api.nvim_buf_add_highlight(bufnr, -1, "RenamePrompt", 0, 0, #rename_prompt)
     vim.fn.prompt_setprompt(bufnr, rename_prompt)
     local winnr =
@@ -42,14 +32,14 @@ end
 
 M.callback = function()
     local new_name = vim.trim(vim.fn.getline("."):sub(#rename_prompt + 1, -1))
-    local vars = vim.api.nvim_buf_get_var(0, "rename_vars")
     vim.cmd [[stopinsert]]
     vim.cmd [[bd!]]
-    if #new_name == 0 or new_name == vars.current_name then
+    if #new_name == 0 or new_name == vim.fn.expand("<cword>") then
         return
     end
-    vars.params.newName = new_name
-    vim.lsp.buf_request(vars.orig_bufnr, "textDocument/rename", vars.params)
+    local params = vim.lsp.util.make_position_params()
+    params.newName = new_name
+    vim.lsp.buf_request(0, "textDocument/rename", params)
 end
 
 return M
