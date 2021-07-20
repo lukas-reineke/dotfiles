4b7bc676c17fc1225f7f134fd19e6d46e5610631
Lukas Reineke <lukas.reineke@protonmail.com>
2021-07-21 00:31:29 +0900

    formatted with shfmt

--- scripts/product-board.sh
+++ scripts/product-board.sh
@@ -3,26 +3,20 @@
 function productboard_features() {
     NEXT="https://api.productboard.com/features"
 
-    while [ "$NEXT" != "" ]
-    do
-        CURL=$(curl --location --silent --request GET --header "Authorization: Bearer ${PRODUCTBOARD_TOKEN}" --header "x-version: 1" $NEXT)
-    
-        JQ=$(jq -r '.data[]|"\(.name)\t\(.id)" | gsub("\n"; " ")' <<< "$CURL")
+    while [[ "$NEXT" != "" ]]; do
+        CURL=$(curl --silent --header "Authorization: Bearer ${PRODUCTBOARD_TOKEN}" --header "x-version: 1" $NEXT)
 
-        if [[ ! "$?" -eq 0 ]]; then
-            echo "parsing the result has failed"
-            exit 1
-        fi
+        JQ=$(jq -r '.data[]|"\(.name)\t\(.id)" | gsub("\n"; " ")' <<<"$CURL")
 
         echo -e "$JQ" | column -t -s $'\t'
-        NEXT=$(jq -r '.links.next' <<< "$CURL")
+        NEXT=$(jq -r '.links.next' <<<"$CURL")
     done
 }
 
-FEATURES=$(productboard_features| fzf --height 20% --reverse --multi)
+FEATURES=$(productboard_features | fzf --height 20% --reverse --multi)
 
 if [[ -n $FEATURES ]]; then
-    for id in $(echo -e "$FEATURES" | awk '{print $NF}'); do
-        echo resolve "$id"
+    for ID in $(echo -e "$FEATURES" | awk '{print $NF}'); do
+        echo resolve "$ID"
     done
 fi
