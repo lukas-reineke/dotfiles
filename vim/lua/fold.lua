4543171aed97e1b43cbf6631aec53eb4414fc7e3
Lukas Reineke <lukas.reineke@protonmail.com>
2021-07-30 18:28:07 +0900

    fixed fold functions

--- vim/lua/fold.lua
+++ vim/lua/fold.lua
@@ -1,10 +1,13 @@
 local M = {}
+local utils = require "utils"
 
 M.close_all = function()
-    if vim.wo.diff and #vim.api.nvim_list_wins() > 1 then
+    if vim.wo.diff and #vim.api.nvim_list_wins() > 1 and vim.fn.foldlevel(vim.fn.getcurpos()[2]) == 0 then
+        local view = vim.fn.winsaveview()
         local win = vim.api.nvim_get_current_win()
-        vim.cmd [[windo normal! zm]]
+        vim.cmd [[windo normal! zM]]
         vim.api.nvim_set_current_win(win)
+        vim.fn.winrestview(view)
     end
 end
 
@@ -13,7 +16,8 @@ _G.foldtext = function()
     local line_count = vim.v.foldend - vim.v.foldstart + 1
     local _, whitespace_count = line:find("^%s+")
 
-    return line:sub(1, whitespace_count) .. "祉[" .. line_count .. "] " .. vim.trim(line) .. " "
+    return utils._if(whitespace_count, line:sub(1, whitespace_count), "") ..
+        "祉[" .. line_count .. "] " .. vim.trim(line) .. " "
 end
 
 return M
