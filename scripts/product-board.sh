bcc6dfbf8ead2d3102283a1df88f7bdf0668a2cc
Lukas Reineke <lukas.reineke@protonmail.com>
2022-03-16 15:14:55 +0900

    use productboard api filtering

--- scripts/product-board.sh
+++ scripts/product-board.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 function productboard_features() {
-    NEXT="https://api.productboard.com/features"
+    for STATUS in "Planned" "In%20Progress"; do
+        NEXT="https://api.productboard.com/features?status.name=$STATUS"
 
-    while [[ "$NEXT" != "" ]]; do
-        CURL=$(curl --silent --header "Authorization: Bearer ${PRODUCTBOARD_TOKEN}" --header "x-version: 1" $NEXT)
+        while [[ "$NEXT" != "" ]]; do
+            CURL=$(curl --silent --header "Authorization: Bearer ${PRODUCTBOARD_TOKEN}" --header "x-version: 1" "$NEXT")
 
-        JQ=$(jq -r '.data[]|"\(.name)\t\(.id)" | gsub("\n"; " ")' <<<"$CURL")
+            JQ=$(jq -r '.data[]|"\(.name)\t\(.id)" | gsub("\n"; " ")' <<<"$CURL")
 
-        echo -e "$JQ" | column -t -s $'\t'
-        NEXT=$(jq -r '.links.next' <<<"$CURL")
+            echo -e "$JQ" | column -t -s $'\t'
+            NEXT=$(jq -r '.links.next' <<<"$CURL")
+        done
     done
 }
 
