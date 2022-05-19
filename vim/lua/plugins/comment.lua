4f851e240293da28048dc91d87ae795a33eb49d2
Lukas Reineke <lukas.reineke@protonmail.com>
2022-05-19 13:03:00 +0900

    add tsx comment hook

--- vim/lua/plugins/comment.lua
+++ vim/lua/plugins/comment.lua
@@ -1,1 +1,23 @@
-require("Comment").setup()
+require("Comment").setup {
+    pre_hook = function(ctx)
+        if vim.bo.filetype == "typescriptreact" then
+            local U = require "Comment.utils"
+
+            -- Determine whether to use linewise or blockwise commentstring
+            local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
+
+            -- Determine the location where to calculate commentstring from
+            local location = nil
+            if ctx.ctype == U.ctype.block then
+                location = require("ts_context_commentstring.utils").get_cursor_location()
+            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+                location = require("ts_context_commentstring.utils").get_visual_start_location()
+            end
+
+            return require("ts_context_commentstring.internal").calculate_commentstring {
+                key = type,
+                location = location,
+            }
+        end
+    end,
+}
