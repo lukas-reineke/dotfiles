0c0e009e7d298e4eb2cfffaaf869ccd4b235ea49
Lukas Reineke <lukas.reineke@protonmail.com>
2022-07-27 17:10:44 +0900

    remove unused highlights from markdown

--- vim/after/queries/markdown/highlights.scm
+++ vim/after/queries/markdown/highlights.scm
@@ -2,22 +2,22 @@
 (fenced_code_block (info_string) @MDTSFencedCodeBlockInfo) @MDTSFencedCodeBlock
 (fenced_code_block_delimiter) @MDTSFencedCodeBlock
 (
-    (atx_heading (atx_h1_marker) @MDTSHeadlineMarker) @OrgTSHeadlineLevel1
+    (atx_heading (atx_h1_marker)) @OrgTSHeadlineLevel1
     (set! "priority" 2050)
 )
 (
-    (atx_heading (atx_h2_marker) @MDTSHeadlineMarker) @OrgTSHeadlineLevel2
+    (atx_heading (atx_h2_marker)) @OrgTSHeadlineLevel2
     (set! "priority" 2050)
 )
 (
-    (atx_heading (atx_h3_marker) @MDTSHeadlineMarker) @OrgTSHeadlineLevel3
+    (atx_heading (atx_h3_marker)) @OrgTSHeadlineLevel3
     (set! "priority" 2050)
 )
 (
-    (atx_heading (atx_h4_marker) @MDTSHeadlineMarker) @OrgTSHeadlineLevel4
+    (atx_heading (atx_h4_marker)) @OrgTSHeadlineLevel4
     (set! "priority" 2050)
 )
 (
-    (atx_heading (atx_h5_marker) @MDTSHeadlineMarker) @OrgTSHeadlineLevel5
+    (atx_heading (atx_h5_marker)) @OrgTSHeadlineLevel5
     (set! "priority" 2050)
 )
