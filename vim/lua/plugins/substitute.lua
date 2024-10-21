4288e553129559c74a5022ac46e1f4d3a64dad23
Lukas Reineke <lukas@reineke.jp>
2024-10-21 17:43:32 +0900

    remove new neovim keymaps

--- vim/lua/plugins/substitute.lua
+++ vim/lua/plugins/substitute.lua
@@ -1,10 +1,14 @@
 return {
     "gbprod/substitute.nvim",
-    keys = { "gr", "grr" },
     config = function()
         require("substitute").setup {
             highlight_substituted_text = { timer = 150 },
         }
+
+        vim.keymap.del("n", "grn")
+        vim.keymap.del("n", "gra")
+        vim.keymap.del("n", "grr")
+        vim.keymap.del("n", "gri")
 
         vim.keymap.set("n", "gr", require("substitute").operator, { noremap = true })
         vim.keymap.set("n", "grr", function()
